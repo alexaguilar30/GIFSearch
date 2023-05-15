27480944b49697f7fa789da211993df80e762e26
alexaguilar30 <manuel300413@gmail.com>
2023-05-15 16:01:56 -0600

    Pruebas unitarias

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
 export const GifItem = ({title, url}) => { 
 
@@ -9,4 +9,9 @@ export const GifItem = ({title, url}) => {
 
     </div>
   )
+}
+
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 }
